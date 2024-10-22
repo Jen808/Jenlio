2174ef14c9e02f7a2da76f0810873c7e3aecfff5
Jennifer Jung <chorong@umich.edu>
2024-10-21 21:48:20 -0400

    change interface oh home-main

--- script.js
+++ script.js
@@ -232,4 +232,9 @@ document.head.appendChild(link);
 
 
 
+// const customCursor = document.querySelector('.custom-cursor');
 
+// document.addEventListener('mousemove', (e) => {
+//   customCursor.style.left = `${e.pageX}px`;
+//   customCursor.style.top = `${e.pageY}px`;
+// });
